fd6de4983a95a07495bf24427bd3572075fb18d4
Opilane TTHK <opilane@tthk.ee>
2019-09-13 18:31:31 +0300

    Operators

---
+++ Operators/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 2;
+           // Console.WriteLine(x + y);
+            /*Console.WriteLine(x - y);
+            Console.WriteLine(x * y);
+            Console.WriteLine(x / y);*/
+            //Console.ReadLine();
+            Console.WriteLine(x=y);
+            Console.WriteLine(x +=y);
+            Console.WriteLine(x -= y);
+            Console.WriteLine( x%y);
+            Console.ReadLine();
+            //|| OR (pipes)
+
+
+
+        }
+    }
+}
